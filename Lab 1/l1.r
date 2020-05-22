8a363bca72d8ee670f870647fbe1dfb83a929cc0
Benjamín <benjamin.munoz.tapia@gmail.com>
2020-05-22 19:21:30 -0400

    pruebas de independencia

--- Lab 1/l1.r
+++ Lab 1/l1.r
@@ -92,47 +92,14 @@ plot.barchart<-function(
     theme(panel.border = element_blank(),panel.background= element_blank())
 }
 
+#tablas de contingencia para las clases y cada caracteristica
+contingency.date <- table(data$classname,data$date)
+contingency.precip <- table(data$classname,data$precip)
+contingency.temp <- table(data$classname,data$temp)
+contingency.areadamaged <- table(data$classname,data$`area-damaged`)
 
-barplot<-ggarrange(
+temp.chi<- chisq.test(contingency.temp,simulate.p.value = TRUE)
+areadamaged.chi<- chisq.test(contingency.areadamaged, simulate.p.value = TRUE)
 
-p1<-  plot.barchart(df_date,df_date$Var1,df_date$Freq,"Mes","y","Date"),
-p2<-  plot.barchart(df_plant_stand,df_plant_stand$Var1,df_plant_stand$Freq,"Plant stand","y","Plant stand"),
-p3<-  plot.barchart(df_area_damaged,df_area_damaged$Var1,df_area_damaged$Freq,"x","y","Area damaged"),
-p4<-  plot.barchart(df_canker_lesion,df_canker_lesion$Var1,df_canker_lesion$Freq,"x","y","Canker lesion"),
-p5<-  plot.barchart(df_precip,df_precip$Var1,df_precip$Freq,"x","y","Precip"),
-p6<-  plot.barchart(df_temp,df_temp$Var1,df_temp$Freq,"x","y","Temp"),
-p7<-  plot.barchart(df_hail,df_hail$Var1,df_hail$Freq,"x","y","Hail"),
-p8<-  plot.barchart(df_crop_hist,df_crop_hist$Var1,df_crop_hist$Freq,"x","y","Crop Hist"),
-p9<-  plot.barchart(df_severity,df_severity$Var1,df_severity$Freq,"x","y","Severity"),
-p10<-  plot.barchart(df_seed_tmt,df_seed_tmt$Var1,df_seed_tmt$Freq,"Mes","y","Seed tmt"),
-
-p11<-  plot.barchart(df_germination,df_germination$Var1,df_germination$Freq,"x","y","Germination"),
-p12<-  plot.barchart(df_plant_growth,df_plant_growth$Var1,df_plant_growth$Freq,"x","y","Area damaged"),
-p13<-  plot.barchart(df_leaves,df_leaves$Var1,df_leaves$Freq,"x","y","Leaves"),
-p14<-  plot.barchart(df_leafspots_halo,df_leafspots_halo$Var1,df_leafspots_halo$Freq,"x","y","Leafspots Halo"),
-p15<-  plot.barchart(df_leafspots_marg,df_leafspots_marg$Var1,df_leafspots_marg$Freq,"x","y","Leafspost Marg"),
-p16<-  plot.barchart(df_leafspot_size,df_leafspot_size$Var1,df_leafspot_size$Freq,"x","y","Leafspot Size"),
-p17<-  plot.barchart(df_leaf_shread,df_leaf_shread$Var1,df_leaf_shread$Freq,"x","y","Leaf Shread"),
-p18<-  plot.barchart(df_leaf_malf,df_leaf_malf$Var1,df_leaf_malf$Freq,"x","y","Leaf Malf"),
-
-p19<-  plot.barchart(df_leaf_mild,df_leaf_mild$Var1,df_leaf_mild$Freq,"x","y","Leaf Mild"),
-p20<-  plot.barchart(df_stem,df_stem$Var1,df_stem$Freq,"x","y","Stem"),
-p21<-  plot.barchart(df_lodging,df_lodging$Var1,df_lodging$Freq,"x","y","Lodging"),
-p22<-  plot.barchart(df_stem_cankers,df_stem_cankers$Var1,df_stem_cankers$Freq,"x","y","Stem Cankers"),
-p23<-  plot.barchart(df_fruiting_bodies,df_fruiting_bodies$Var1,df_fruiting_bodies$Freq,"x","y","Fruiting Bodies"),
-p24<-  plot.barchart(df_external_decay,df_external_decay$Var1,df_external_decay$Freq,"x","y","External Bodies"),
-p25<-  plot.barchart(df_mycelium,df_mycelium$Var1,df_mycelium$Freq,"x","y","Mycelium"),
-p26<-  plot.barchart(df_int_discolor,df_int_discolor$Var1,df_int_discolor$Freq,"x","y","Int Discolor"),
-p27<-  plot.barchart(df_fruit_pods,df_fruit_pods$Var1,df_fruit_pods$Freq,"x","y","Fruit Pods"),
-
-p28<-  plot.barchart(df_fruit_spots,df_fruit_spots$Var1,df_fruit_spots$Freq,"x","y","Fruit Spots"),
-p29<-  plot.barchart(df_seed,df_seed$Var1,df_seed$Freq,"x","y","Seed"),
-p30<-  plot.barchart(df_mold_growth,df_mold_growth$Var1,df_mold_growth$Freq,"x","y","Mold Growth"),
-p31<-  plot.barchart(df_seed_discolor,df_seed_discolor$Var1,df_seed_discolor$Freq,"x","y","Seed Discolor"),
-p32<-  plot.barchart(df_seed_size,df_seed_size$Var1,df_seed_size$Freq,"x","y","Seed Size"),
-p33<-  plot.barchart(df_shriveling,df_shriveling$Var1,df_shriveling$Freq,"x","y","Shriveling"),
-p34<-  plot.barchart(df_roots,df_roots$Var1,df_roots$Freq,"x","y","Roots"),
-
-nrow=4, ncol=4
-
-)
+temp.chi.residuals <- temp.chi$residuals
+areadamaged.chi.residuals <- areadamaged.chi$residuals
