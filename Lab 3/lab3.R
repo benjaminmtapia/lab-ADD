f340e1aee29380819549dc8446567f8ddb981f39
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2020-08-18 21:42:56 -0400

    Avances

--- Lab 3/lab3.R
+++ Lab 3/lab3.R
@@ -27,6 +27,37 @@ boxplot1 <- ggboxplot(data = data, x="classname", y ="area-damaged",color="class
 #algoritmo apriori
 #aplicar algoritmo para: todo el dataset, enfermedades 1 2 y 3
 
-rules = apriori(data, parameter = list(support = 0.2,minlen=2,maxlen=2,target="rules"))
+#Apriori para todo el dataset
+rulesAllData = apriori(data,
+                       parameter = list(support = 0.2,minlen = 2, maxlen= 2, target="rules")) #Ver si se puede agregar lo de la clase
+#Como inspect es una especie de print, si se pone aqui en el ranking no se muestra
+result_rulesAllData <- sort(x = rulesAllData, decreasing = TRUE, by  = "confidence")
+inspect(result_rulesAllData)
 
-inspect(sort(x = rules, decreasing = TRUE, by = "confidence"))
+#Apriori de ranking 10 reglas by lift
+result_liftRanking <- inspect(head(result_rulesAllData, n = 10, by = "lift"))
+
+#Top 20 itemsets de mayor a menor soporte
+top20a <- sort(result_rulesAllData, by = "support", decreasing = TRUE)[1:20]
+inspect(top20a) #Muestra los que tienen soporte de 1
+#Grafico: (NO SE MUESTRA)
+graph_20a <- as(top20a, Class = "data.frame") %>%
+  ggplot(aes(x = reorder(items,support), y = support)) + 
+  geom_col() + 
+  coord_flip() + 
+  labs(title = "Itemsets mas frecuentes \ Mayor a menor soporte", x = "itemsets") +
+  theme_bw()
+
+#Top 20 itemsets de menor a mayor soporte
+top20b <- sort(result_rulesAllData, by = "support", decreasing = FALSE)[1:20]
+inspect(top20b) #Muestra los que tienen soporte de 1
+
+#Apriori phythoptora-rot
+#dataPhytho = data[31:46,]
+#rulesPhytho = apriori(dataPhytho,
+#                       parameter = list(support = 0.2,minlen = 1, maxlen= 2, target="rules")) #Ver si se puede agregar lo de la clase
+#Como inspect es una especie de print, si se pone aqui en el ranking no se muestra
+#result_rulesPhytho <- sort(x = rulesPhytho, decreasing = TRUE, by  = "confidence")
+#inspect(result_rulesPhytho)
+
+#Apriori brown-spot
